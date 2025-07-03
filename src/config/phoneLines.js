6300eb652c4e570270fb281c6c732e55a31e671c
Dxz23 <111670397+Dxz23@users.noreply.github.com>
2025-07-02 20:09:09 -0700

    Create phoneLines.js

---
+++ src/config/phoneLines.js
@@ -0,0 +1,6 @@
+// src/config/phoneLines.js
+export default {
+  '606720735854541': { alias: 'linea1', descripcion: 'Número viejo' },
+  '559835943885255': { alias: 'linea2', descripcion: 'Número nuevo CDMX' },
+  '540626332472824': { alias: 'linea3', descripcion: 'Soporte / backup' }
+};
